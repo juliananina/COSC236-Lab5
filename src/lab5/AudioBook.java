e2c486fcb96e4e7d5c25bcddc43aee71da06ff69
guntash499 <guntash.brarltd@gmail.com>
2024-11-21 14:35:58 -0800

    Added new Book types

---
+++ src/lab5/AudioBook.java
@@ -0,0 +1,27 @@
+package lab5;
+
+public class AudioBook implements Book {
+    private String title;
+    private boolean isAvailable;
+
+    public AudioBook(String title) {
+        this.title = title;
+        this.isAvailable = true; }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+    @Override
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+    @Override
+    public boolean getIsAvailable() {
+        return this.isAvailable;
+    }
+    @Override
+    public String toString() {
+        return "AudioBook [Title: " + title + ", Available: " + isAvailable + "]";
+    }
+}
